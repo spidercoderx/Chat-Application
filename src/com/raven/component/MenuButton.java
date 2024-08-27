b5db3d2f1dafd8a1a9215f5b2ce060f6d99d5071
spidercoderx <suyashjain2202@gmail.com>
2024-08-27 23:10:37 +0530

    commit successfull

---
+++ src/com/raven/component/MenuButton.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.raven.component;
+
+import javax.swing.JButton;
+
+public class MenuButton extends JButton {
+
+    public MenuButton() {
+        setContentAreaFilled(false);
+    }
+
+}
